38c7750809f84a6692c8730c6efcd557b1de9d9c
Ebube Ukaere <145124966+GrandMaestro22@users.noreply.github.com>
2026-01-02 03:08:49 +0100

    Project 2: A Simple login page with links.

--- project-1/script.js
+++ project-1/script.js
@@ -15,7 +15,23 @@ setInterval(() => {
     let s = newDate().getSeconds();
     let ap = h>=12 ? 'PM' : 'AM';
 
-    if(h>12){
-        h = h -12
+    if(h > 12){
+        h = h - 12;
     }
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+
+    hours.innerHTML = h + 'Hours';
+    minuites.innerHTML = m + 'Minuites';
+    seconds.innerHTML = s + 'Seconds';
+    ampm.innerHTML = ap;
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    dotH.style.transform = `rotate(${h * 30}deg)`;
+    dotM.style.transform = `rotate(${m * 6}deg)`;
+    dotS.style.transform = `rotate(${s * 6}deg)`;
 }, 1000);
